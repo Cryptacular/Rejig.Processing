091ef9d287b5a390b3f2392e60c3ce80531255c2
Nick Mertens <nick@thecrypt.co.nz>
2023-01-15 16:28:00 +1300

    Add tests for schema validation functions

--- test/schema.spec.ts
+++ test/schema.spec.ts
@@ -1,19 +1,87 @@
-import { workflowSchema } from "../src/models/Workflow";
+import {
+  isValid,
+  validate,
+  Workflow,
+  workflowSchema,
+} from "../src/models/Workflow";
 
 describe("Workflow schema", () => {
-  it("is not valid when validating an empty object", async () => {
-    const workflow = {};
+  describe("> isValid", () => {
+    it("returns false when validating an empty object", async () => {
+      const workflow = {};
 
-    expect(() =>
-      workflowSchema.validateSync(workflow, { strict: true })
-    ).toThrow();
+      const result = await isValid(workflow);
+      expect(result).toBe(false);
+    });
+
+    it("returns false when 'size' has the wrong type", async () => {
+      const workflow = { size: "hello" };
+
+      const result = await isValid(workflow);
+      expect(result).toBe(false);
+    });
+
+    it("returns true when validating an valid object", async () => {
+      const workflow: Workflow = {
+        size: { width: 100, height: 200 },
+      };
+
+      const result = await isValid(workflow);
+      expect(result).toBe(true);
+    });
   });
 
-  it("is not valid when 'size' has the wrong type", async () => {
-    const workflow = { size: "hello" };
+  describe("> validate", () => {
+    it("throws when validating null", async () => {
+      const workflow = null;
 
-    expect(() =>
-      workflowSchema.validateSync(workflow, { strict: true })
-    ).toThrow();
+      try {
+        await validate(workflow);
+        // Fallback to make sure the test fails
+        expect(true).toBe(false);
+      } catch (e: any) {
+        expect(e.errors).toHaveLength(1);
+      }
+    });
+
+    it("throws when validating an empty object", async () => {
+      const workflow = {};
+
+      try {
+        await validate(workflow);
+        // Fallback to make sure the test fails
+        expect(true).toBe(false);
+      } catch (e: any) {
+        expect(e.errors).toHaveLength(1);
+        expect(e.errors[0]).toContain("size is a required field");
+      }
+    });
+
+    it("throws when 'size' has the wrong type", async () => {
+      const workflow = { size: "hello" };
+
+      try {
+        await validate(workflow);
+        // Fallback to make sure the test fails
+        expect(true).toBe(false);
+      } catch (e: any) {
+        expect(e.errors).toHaveLength(1);
+        expect(e.errors[0]).toContain(
+          'size must be a `object` type, but the final value was: `"hello"`.'
+        );
+      }
+    });
+
+    it("does not throw when object is valid", async () => {
+      const workflow: Workflow = {
+        size: { width: 100, height: 200 },
+      };
+
+      try {
+        await validate(workflow);
+      } catch (e: any) {
+        expect(e).toBeFalsy();
+      }
+    });
   });
 });
